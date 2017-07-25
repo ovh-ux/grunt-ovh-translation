7b6a12747f9a235f03db36e3391afca6971f88d0
jean-christophe.alleman <jean-christophe.alleman@corp.ovh.com>
2017-07-25 11:27:59 +0200

    chore(Gruntfile): add Makefile and update Gruntfile for release

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,10 +13,22 @@ module.exports = function(grunt) {
         },
         nodeunit: {
             tests: ['test/test_*.js']
+        },
+        // To release
+        bump: {
+            options: {
+                pushTo: "origin",
+                files: [
+                    "package.json"
+                ],
+                updateConfigs: ["pkg"],
+                commitFiles: ["-a"]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-bump');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
     grunt.registerTask('test', [
@@ -26,4 +38,17 @@ module.exports = function(grunt) {
 
 
     grunt.registerTask('default', ['test']);
+
+    // Increase version number. Type = minor|major|patch
+    grunt.registerTask("release", "Release", function () {
+        var type = grunt.option("type");
+
+        if (type && ~["patch", "minor", "major"].indexOf(type)) {
+            grunt.task.run(["bump-only:" + type]);
+        } else {
+            grunt.verbose.or.write("You try to release in a weird version type [" + type + "]").error();
+            grunt.fail.warn("Please try with --type=patch|minor|major");
+        }
+    });
+
 };
